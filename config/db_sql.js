4e22cb9875632b8cc58641d9c545de5a55296d76
ksh030506 <llmm030506@naver.com>
2020-11-05 19:46:17 +0800

    Updata
    API 서버 수정

--- config/db_sql.js
+++ config/db_sql.js
@@ -1,10 +1,32 @@
 var pool = require('./db_connect');
+
+var nickname = 'aaa';
  
 module.exports = function () {
   return {
-    select: function(callback){
+    select_all: function(callback){
       pool.getConnection(function(err, con){
-        var sql = 'select * from TestTable';
+        var sql = 'select User.Nickname, Game.GameID, Game.Kill from Game, User where Game.Nickname = User.Nickname;';
+        con.query(sql, function (err, result, fields) {
+          con.release();
+          if (err) return callback(err);
+          callback(null, result);
+        });
+      });
+    },
+    insert_nick: function(callback){
+      pool.getConnection(function(err, con){
+        var sql = "insert into User values ("+ "nickname" +");";
+        con.query(sql, function (err, result, fields) {
+          con.release();
+          if (err) return callback(err);
+          callback(null, result);
+        });
+      });
+    },
+    select_nickname: function(callback){
+      pool.getConnection(function(err, con){
+        var sql = 'select User.Nickname from User';
         con.query(sql, function (err, result, fields) {
           con.release();
           if (err) return callback(err);
--- server.js
+++ server.js
@@ -11,27 +11,46 @@ app.get('/', (req, res) => {
   res.send('이것은 게임 서버입니다.');
 });
 
-app.get('/database', (req, res) => {
-    res.send('이것은 베이터베이스 테스트입니다.');
-    sql.select(function(err, data){
-        if (err) console.log(err);
-        else console.log(data);
+//#region GET Test
+// app.get('/database', (req, res) => {
+//     res.send('이것은 베이터베이스 테스트입니다.');
+//     sql.select_all(function(err, data){
+//         if (err) console.log(err);
+//         else console.log(data);
        
-        sql.pool.end(function(err){
-          if (err) console.log(err);
-        });
-      });
-      res.end();
-});
+//         sql.pool.end(function(err){
+//           if (err) console.log(err);
+//         });
+//       });
+//       res.end();
+// });
+//#endregion
 
-app.post('/database1', (req, res) => {
-  var uId = req.body;
-  console.log(uId);
-  res.json({
-    "code":200,
-    "uId": uId
+// app.post('/database2', (req, res) => {
+//   var nickname = req.body;
+//   console.log(uId);
+//   res.json({
+//     "code":200,
+//     "nickname": nickname
+//   });
+// });
+
+
+app.post('/nickname', (req, res) => {
+  sql.select_nickname(function(err, data){
+    if (err) {
+      
+    }
+    else console.log(data);
+   
+    sql.pool.end(function(err){
+      if (err) console.log(err);
+    });
   });
 });
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
