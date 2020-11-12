127536ff00f3cb205f5e7295ff2f13f1756975a1
ksh030506 <llmm030506@naver.com>
2020-11-12 14:02:28 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const conn = mysql.createConnection({
   host     : 'localhost',
@@ -9,8 +8,10 @@ const conn = mysql.createConnection({
   password : 'password123@',
   database : 'UnityGround'
 });
-const cors = require('cors');
+
 let User = require('./model/info');
+
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -19,72 +20,72 @@ app.use(bodyParser.urlencoded({
 
 
 app.get('/', (req, res) => {
-  conn.query(`select * from user_info`, function(error, results, fields) {
-    if(error) {
-      console.log(error);
-    }
-    return res.json(results);
+    conn.query(`select * from user_info`, function(error, results, fields) {
+      if(error) {
+        console.log(error);
+      }
+      return res.json(results);
+    });
   });
-});
-
-//info add
-app.post('/infoadd', (req, res) => {
-  let score = User.score = req.body.score;
-  let user_kill = User.user_kill = req.body.user_kill;
-  let time_user = User.time_user = req.body.time_user;
-  console.log(User);
-
-  if(score && user_kill) {
-    conn.query(`INSERT INTO user_info(score, user_kill, time_user) VALUES (${score}, ${user_kill}, ${time_user});`,
-      function(error, results, fields){
-        if(error) {
-          console.log(error);
-        }
-        res.status(200).json({
-          'status': 200,
-          'msg': 'success',
-          'user': User
+  
+  //info add
+  app.post('/infoadd', (req, res) => {
+    let score = User.score = req.body.score;
+    let user_kill = User.user_kill = req.body.user_kill;
+    let time_user = User.time_user = req.body.time_user;
+    console.log(User);
+  
+    if(score && user_kill) {
+      conn.query(`INSERT INTO user_info(score, user_kill, time_user) VALUES (${score}, ${user_kill}, ${time_user});`,
+        function(error, results, fields){
+          if(error) {
+            console.log(error);
+          }
+          res.status(200).json({
+            'status': 200,
+            'msg': 'success',
+            'user': User
+          });
         });
+    } else {
+      res.status(400).json({
+            'status': 400,
+            'msg': '값이 부족합니다.'
+          });
+    }
+  });
+  
+  
+  //총 스코어
+  app.get('/score', function(req, res){
+    conn.query(`select SUM(score) as '총 스코어' from user_info;`, 
+    function(error, results, fields){
+      if(error){
+        console.log(error);
+      }
+      res.status(200).json({
+        "code": 200,
+        'msg': 'success',
+        "총 스코어": results
       });
-  } else {
-    res.status(400).json({
-          'status': 400,
-          'msg': '값이 부족합니다.'
-        });
-  }
-});
-
-
-//총 스코어
-app.get('/score', function(req, res){
-  conn.query(`select SUM(score) as '총 스코어' from user_info;`, 
-  function(error, results, fields){
-    if(error){
-      console.log(error);
-    }
-    res.status(200).json({
-      "code": 200,
-      'msg': 'success',
-      "총 스코어": results
     });
   });
-});
-
-
-//총 스코어
-app.get('/user_kill', function(req, res){
-  conn.query(`select SUM(user_kill) as '총 킬' from user_info;`, 
-  function(error, results, fields){
-    if(error){
-      console.log(error);
-    }
-    res.status(200).json({
-      "code": 200,
-      'msg': 'success',
-      "총 킬": results
+  
+  
+  //총 스코어
+  app.get('/user_kill', function(req, res){
+    conn.query(`select SUM(user_kill) as '총 킬' from user_info;`, 
+    function(error, results, fields){
+      if(error){
+        console.log(error);
+      }
+      res.status(200).json({
+        "code": 200,
+        'msg': 'success',
+        "총 킬": results
+      });
     });
   });
-});
 
 
 
